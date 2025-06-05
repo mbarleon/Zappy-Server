e80bd4fec53a630696af708e4a4336160607a510
Mathieu BARLEON <mathieu.barleon@epitech.eu>
2025-06-05 11:36:42 +0200

    Feat: Added begining of graphic connexion message

--- src/server/connect_client.c
+++ src/server/connect_client.c
@@ -62,26 +62,77 @@ static void connect_client_err(zap_srv_player_t *client,
     THROW(CEXTEND_EXCEPTION_INVALID_ARGUMENT);
 }
 
-void connect_client(zap_srv_player_t *client, zap_srv_parsed_context_t *ctxt)
+static void send_player_connect_message(zap_srv_player_t *client,
+    zap_srv_parsed_context_t *ctxt, size_t slots_in_team)
 {
     char *block;
-    size_t slots_in_team;
-    size_t team_len = count_team_len(client);
 
-    client->team = safe_calloc(team_len + 1, sizeof(char), NULL);
-    memcpy(client->team, client->buf, team_len);
-    if (!team_exists(client->team, ctxt))
-        connect_client_err(client, ZAP_SRV_JOIN_TEAM_ERR);
-    slots_in_team = compute_slots_in_team(client->team, ctxt);
-    if (slots_in_team <= 0)
-        connect_client_err(client, ZAP_SRV_JOIN_FULL_TEAM);
-    CEXTEND_LOG(CEXTEND_LOG_INFO, fetch_string(ZAP_SRV_JOIN_TEAM),
-        client->sock.fd, client->team);
     block = snprintf_alloc("%ld\n%ld %ld\n", slots_in_team - 1, ctxt->map.x,
         ctxt->map.y);
     decrease_team_slots(client->team, ctxt);
     if (block) {
         send_client(block, &client->sock);
         free(block);
+    }
+}
+
+static void send_bct(zap_srv_parsed_context_t *ctxt, zap_srv_player_t *client,
+    size_t i, size_t j)
+{
+    char *block;
+    size_t quantity_table[ZAP_SRV_ELEMENTS_QUANTITY] = {0};
+
+    for (const zap_srv_elements_list_t *tmp = ctxt->map.elements[i][j]; tmp;
+        tmp = tmp->next) {
+        quantity_table[tmp->element] += 1;
+    }
+    block = snprintf_alloc("bct %ld %ld %ld %ld %ld %ld %ld %ld %ld\n", i, j,
+        quantity_table[0], quantity_table[1], quantity_table[2],
+        quantity_table[3], quantity_table[4], quantity_table[5],
+        quantity_table[6]);
+    if (block) {
+        send_client(block, &client->sock);
+        free(block);
+    }
+}
+
+static void send_graphic_connect_message(zap_srv_player_t *client,
+    zap_srv_parsed_context_t *ctxt)
+{
+    char *block;
+
+    block = snprintf_alloc("msz %ld %ld\nsgt %ld\n", ctxt->map.x, ctxt->map.y,
+        ctxt->server.frequency);
+    if (block) {
+        send_client(block, &client->sock);
+        free(block);
+    }
+    for (size_t i = 0; i < ctxt->map.x; ++i) {
+        for (size_t j = 0; j < ctxt->map.y; ++j) {
+            send_bct(ctxt, client, i, j);
+        }
+    }
+}
+
+void connect_client(zap_srv_player_t *client, zap_srv_parsed_context_t *ctxt)
+{
+    size_t slots_in_team;
+    size_t team_len = count_team_len(client);
+
+    client->team = safe_calloc(team_len + 1, sizeof(char), NULL);
+    memcpy(client->team, client->buf, team_len);
+    if (!team_exists(client->team, ctxt)) {
+        connect_client_err(client, ZAP_SRV_JOIN_TEAM_ERR);
+    }
+    slots_in_team = compute_slots_in_team(client->team, ctxt);
+    if (slots_in_team <= 0) {
+        connect_client_err(client, ZAP_SRV_JOIN_FULL_TEAM);
+    }
+    CEXTEND_LOG(CEXTEND_LOG_INFO, fetch_string(ZAP_SRV_JOIN_TEAM),
+        client->sock.fd, client->team);
+    if (strcmp(client->team, "GRAPHIC") == 0) {
+        send_graphic_connect_message(client, ctxt);
+    } else {
+        send_player_connect_message(client, ctxt, slots_in_team);
     }
 }
